2c55e24abc4ee9195bd5575f6a899b3420fb085e
kia <labeast0021@gmail.com>
2024-07-23 12:24:06 -0700

    majority element

--- leetcode.js
+++ leetcode.js
@@ -146,3 +146,20 @@
 // Given an array nums of size n, return the majority element.
 
 // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array
+var majorityElement = function(nums) {
+    nums.sort((a,b) => a < b)
+    console.log(nums)
+    let current = nums[0] , count = 1 , size = nums.length , half = size / 2 , k
+
+    for (i = 1 ; i < size ; i++){
+        nums[i] === current ? count ++ : [current , count] = [nums[i] , 1]
+        console.log(current , "dddd" , count , "ffff" , )
+        if (count == half ) {
+            k = current
+            break
+        }
+    }
+    return k
+    
+};
+majorityElement([3,2,3])
